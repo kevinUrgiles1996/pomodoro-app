1051438ab03bc54e21b510948bfe6937a39df4ec
Kevin Urgilés <kevinurgiles1996@gmail.com>
2020-05-07 18:12:57 -0500

    Pomodoro App - Day 2

--- main.js
+++ main.js
@@ -4,15 +4,13 @@ const resetButton = document.getElementById('resetButton');
 const shortBreakButton = document.getElementById('shortBreakButton');
 const longBreakButton = document.getElementById('longBreakButton');
 
+const configButton = document.querySelector('.configButton');
+const modalContainer = document.querySelector('.modal-container');
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.close-button');
+
 let totalSeconds;
 let intervalId;
-
-const endTimer = () => {
-  clearInterval(intervalId);
-  setTimeout(() => {
-    alert('Timer has ended');
-  }, 1000);
-};
 
 const countDown = () => {
   if (totalSeconds === 0) endTimer();
@@ -33,6 +31,11 @@ const startContDown = (minutes) => {
   intervalId = setInterval(countDown, 1000);
 };
 
+const endTimer = () => {
+  clearInterval(intervalId);
+  setTimeout(() => alert('Timer has ended'), 1000);
+};
+
 startButton.addEventListener('click', (e) => startContDown(25));
 shortBreakButton.addEventListener('click', (e) => startContDown(5));
 longBreakButton.addEventListener('click', (e) => startContDown(15));
@@ -40,4 +43,16 @@ longBreakButton.addEventListener('click', (e) => startContDown(15));
 resetButton.addEventListener('click', (e) => {
   clearInterval(intervalId);
   timer.textContent = '00:00';
+});
+
+configButton.addEventListener('click', (e) => {
+  modalContainer.classList.add('visible');
+});
+
+modalContainer.addEventListener('click', (e) => {
+  modalContainer.classList.remove('visible');
+});
+
+modal.addEventListener('click', (e) => {
+  e.stopPropagation();
 });
