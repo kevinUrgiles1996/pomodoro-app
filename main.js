c59cfb6df860bb1b3efe6a9ba14f1307de768a7b
Kevin Urgilés <kevinurgiles1996@gmail.com>
2020-05-07 08:42:04 -0500

    Pomodoro App - Day 2

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ const longBreakButton = document.getElementById('longBreakButton');
 let totalSeconds;
 let intervalId;
 
+const endTimer = () => {
+  clearInterval(intervalId);
+  setTimeout(() => {
+    alert('Timer has ended');
+  }, 1000);
+};
+
 const countDown = () => {
-  if (totalSeconds === 0) {
-    clearInterval(intervalId);
-    setTimeout(() => {
-      alert('Timer has ended');
-    }, 1000);
-  }
+  if (totalSeconds === 0) endTimer();
 
   let minutes = Math.floor(totalSeconds / 60);
   let seconds = totalSeconds % 60;
@@ -25,25 +27,15 @@ const countDown = () => {
   totalSeconds--;
 };
 
-const startContDown = () => {
+const startContDown = (minutes) => {
   clearInterval(intervalId);
+  totalSeconds = minutes * 60;
   intervalId = setInterval(countDown, 1000);
 };
 
-startButton.addEventListener('click', (e) => {
-  totalSeconds = 25 * 60;
-  startContDown();
-});
-
-shortBreakButton.addEventListener('click', (e) => {
-  totalSeconds = 0.5 * 60;
-  startContDown();
-});
-
-longBreakButton.addEventListener('click', (e) => {
-  totalSeconds = 15 * 60;
-  startContDown();
-});
+startButton.addEventListener('click', (e) => startContDown(25));
+shortBreakButton.addEventListener('click', (e) => startContDown(5));
+longBreakButton.addEventListener('click', (e) => startContDown(15));
 
 resetButton.addEventListener('click', (e) => {
   clearInterval(intervalId);
