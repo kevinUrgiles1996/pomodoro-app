6515bc78d61adc175c1a4af200d8a897b9502c55
Kevin Urgilés <kevinurgiles1996@gmail.com>
2020-05-11 21:20:53 -0500

    Pomodoro App - Day 5

--- main.js
+++ main.js
@@ -19,11 +19,15 @@ let tomatoMinutes = 25;
 let shortBreakMinutes = 5;
 let longBreakMinutes = 15;
 
-// Notifications
+// Notifications and Service Worker registration
 let swRegistration;
+
+// INDEXED DB
+let DB;
 
 let totalSeconds;
 let intervalId;
+let timerType;
 
 const countDown = () => {
   if (totalSeconds === 0) endTimer();
@@ -47,16 +51,20 @@ const startContDown = minutes => {
 const endTimer = () => {
   clearInterval(intervalId);
   showLocalNotification('Pomodoro App', 'Timer has ended', swRegistration);
+  setTimeout(() => saveOrUpdate(), 1000);
 };
 
 startButton.addEventListener('click', e => {
   startContDown(tomatoMinutes);
+  timerType = 'pomodoros';
 });
 shortBreakButton.addEventListener('click', e => {
   startContDown(shortBreakMinutes);
+  timerType = 'shortBreaks';
 });
 longBreakButton.addEventListener('click', e => {
   startContDown(longBreakMinutes);
+  timerType = 'longBreaks';
 });
 
 clearTimeButton.addEventListener('click', e => {
@@ -79,7 +87,7 @@ modal.addEventListener('click', e => {
   e.stopPropagation();
 });
 
-const clearTimer = () => {
+const goToMainMenu = () => {
   modalContainer.classList.remove('visible');
   clearInterval(intervalId);
   timer.textContent = '00:00';
@@ -90,14 +98,14 @@ saveButton.addEventListener('click', e => {
   tomatoMinutes = tomatoMinutesInput.value;
   shortBreakMinutes = shortBreakMinutesInput.value;
   longBreakMinutes = longBreakMinutesInput.value;
-  clearTimer();
+  goToMainMenu();
 });
 
 defaultButton.addEventListener('click', e => {
   tomatoMinutes = 25;
   shortBreakMinutes = 5;
   longBreakMinutes = 15;
-  clearTimer();
+  goToMainMenu();
 });
 
 // NOTIFICATIONS
@@ -129,10 +137,95 @@ const showLocalNotification = async (title, body, swRegistration) => {
 };
 
 const main = async () => {
-  window.stop();
   check();
   swRegistration = await registerServiceWorker();
   await requestNotificationPermission();
 };
 
-main();
+// INDEXED DB
+
+const openDB = () => {
+  let createDB = window.indexedDB.open('pomodoro', 1);
+  createDB.onerror = () => console.error('There was an error openning the DB');
+  createDB.onsuccess = () => (DB = createDB.result);
+  // This method just runs once. Ideal to create Schemas
+  createDB.onupgradeneeded = e => {
+    let db = e.target.result;
+    let objectStore = db.createObjectStore('records', {
+      keyPath: 'date',
+      autoIncrement: false,
+    });
+    objectStore.createIndex('date', 'date', { unique: false });
+    objectStore.createIndex('pomodoros', 'pomodoros', { unique: false });
+    objectStore.createIndex('shortBreaks', 'shortBreaks', { unique: false });
+    objectStore.createIndex('longBreaks', 'longBreaks', { unique: false });
+  };
+};
+
+saveNewRecord = objectStore => {
+  const newRecord = {
+    date: new Date().toLocaleDateString('en-US'),
+    pomodoros: 0,
+    shortBreaks: 0,
+    longBreaks: 0,
+  };
+  newRecord[timerType] = 1;
+  let recordAddRequest = objectStore.add(newRecord);
+  recordAddRequest.onsuccess = () => {
+    console.log('Record saved Succesfully!');
+  };
+  recordAddRequest.onerror = () => {
+    console.log('There was an error saving the record');
+  };
+};
+
+updateRecord = (data, objectStore) => {
+  data[timerType]++;
+  let recordUpdateRequest = objectStore.put(data);
+
+  recordUpdateRequest.onsuccess = () => {
+    console.log('Record updated successfully!');
+  };
+  recordUpdateRequest.onerror = () => {
+    console.log('There was an error updating the record');
+  };
+};
+
+const saveOrUpdate = () => {
+  const key = new Date().toLocaleDateString('en-US');
+  let transaction = DB.transaction(['records'], 'readwrite');
+  let objectStore = transaction.objectStore('records');
+
+  let recordGetRequest = objectStore.get(key);
+
+  recordGetRequest.onsuccess = () => {
+    let data = recordGetRequest.result;
+    if (!data) saveNewRecord(objectStore);
+    else updateRecord(data, objectStore);
+  };
+
+  recordGetRequest.onerror = () => {
+    console.error('There was an error getting the record');
+  };
+
+  transaction.oncomplete = () => {
+    console.log('Transaction completed');
+  };
+  transaction.onerror = () => {
+    console.log('There was an error');
+  };
+};
+
+document.addEventListener('DOMContentLoaded', e => {
+  main();
+  openDB();
+});
+
+// document.querySelector('.getAll').addEventListener('click', e => {
+//   let transaction = DB.transaction(['records'], 'readwrite');
+//   let objectStore = transaction.objectStore('records');
+//   const all = objectStore.getAll();
+//   all.onsuccess = e => {
+//     console.log(e.target.result);
+//   };
+// });
