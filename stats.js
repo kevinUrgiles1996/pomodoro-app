ee1a8af7efe1e1233e9add2dc845aa2772fc023d
Kevin Urgilés <kevinurgiles1996@gmail.com>
2020-05-13 11:51:52 -0500

    Pomodoro App - Day 7

---
+++ stats.js
@@ -0,0 +1,64 @@
+let DB;
+let storedRecords;
+
+const openDB = () => {
+  let createDB = window.indexedDB.open('pomodoro', 1);
+  createDB.onsuccess = () => (DB = createDB.result);
+  createDB.onerror = () => {
+    console.error('There was an error openning the DB');
+  };
+};
+
+const getRecordsFromDB = () => {
+  let transaction = DB.transaction(['records'], 'readwrite');
+  let objectStore = transaction.objectStore('records');
+  const getRecords = objectStore.getAll();
+
+  getRecords.onsuccess = e => {
+    storedRecords = e.target.result;
+    console.log(storedRecords);
+  };
+  getRecords.onerror = e => {
+    console.log('There was an error getting the records');
+  };
+};
+
+const drawChart = () => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: storedRecords.map(record => record.date),
+      datasets: [
+        {
+          label: '# of pomodoros per day',
+          data: storedRecords.map(record => parseInt(record.pomodoros)),
+          borderWidth: 1,
+          borderColor: 'red',
+          backgroundColor: '#fff',
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+};
+
+const main = () => {
+  openDB();
+  setTimeout(getRecordsFromDB, 500);
+  setTimeout(drawChart, 1000);
+  // drawChart();
+};
+
+main();
